1e645851b2939e46b7ceca653b43c0e14d2b8f3a
Shabab Karim <shababkarim93@gmail.com>
2020-12-10 14:03:20 +0600

    Init wedding card

---
+++ index.js
@@ -0,0 +1,9 @@
+anime({
+    targets: '#Layer_1 path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 10000,
+    delay: function(el, i) { return i * 250 },
+    direction: 'start',
+    loop: false
+  })
